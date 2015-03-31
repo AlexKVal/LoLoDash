745de5a66d9f656deb16982306147486dae1ce41
AlexKVal <alexkval@gmail.com>
2015-03-31 12:49:53 +0300

    use object key as username

--- 5_count_the_comments.js
+++ 5_count_the_comments.js
@@ -4,8 +4,8 @@ module.exports = function commentsCounter(comments) {
 
   var groupedByName = _.groupBy(comments, 'username')
 
-  var result = _.map(groupedByName, function (el, indx) {
-    return { username: el[0].username, comment_count: _.size(el)}
+  var result = _.map(groupedByName, function (el, username) {
+    return { username: username, comment_count: _.size(el)}
   })
 
   result = _.sortBy(result, 'comment_count').reverse()
