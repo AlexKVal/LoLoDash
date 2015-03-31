d116b869ccd27645edfdb0d7390783464438e192
AlexKVal <alexkval@gmail.com>
2015-03-31 13:31:02 +0300

    a solution for 6_give_me_an_overview

---
+++ 6_give_me_an_overview.js
@@ -0,0 +1,16 @@
+var _ = require('lodash');
+
+module.exports = function worker(orders) {
+
+  var result = _.reduce(orders, function (acc, order) {
+    acc[order.article] = (acc[order.article] || 0) + order.quantity
+    return acc
+  }, {})
+
+
+  result = _.map(result, function (val, key) {
+    return { article: Number(key), total_orders: val }
+  })
+
+  return _.sortBy(result, 'total_orders').reverse()
+};
